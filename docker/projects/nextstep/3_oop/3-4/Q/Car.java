b1a52d95ddc19f2578eaf8738b426284283d26ee
sueno61 <ueno.s@nifty.com>
2022-03-12 14:10:01 +0900

    ネクストドアーセミナーOOP

---
+++ docker/projects/nextstep/3_oop/3-4/Q/Car.java
@@ -0,0 +1,19 @@
+class Car{
+  //メンバ変数
+  int no;
+  int speed;
+
+  //メソッド
+  void setNo(int n){
+    no = n;
+  }
+  void run(int s){
+    speed = s;
+  }
+  void stop(){
+    speed = 0;
+  }
+  void display(){
+    System.out.println("ナンバー" + no + "の速度は" + speed + "です");
+  }
+}
