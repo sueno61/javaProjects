55d29e162a8e01f681efcdd4f1c66f4e099ee786
TkuUeno <sueno@cs.takushoku-u.ac.jp>
2023-11-06 16:57:15 +0900

    出題変更

--- docker/projects/Programing1/_2023/lesson06/Bmi.java
+++ docker/projects/Programing1/_2023/lesson06/Bmi.java
@@ -8,17 +8,22 @@ public class Bmi {
         input = System.console().readLine("体重(kg)を入力して下さい：");
         weight = Integer.parseInt(input);
 
-        bmi = (int) (weight / Math.pow(height / 100.0, 2.0));
-        System.out.println("あなたのBMIは" + bmi + "です。");
-
-        if (bmi <= 0) {
+        if (height <= 0 || weight <= 0) {
             msg = "身長または体重、あるいはその両方の入力が間違っています。";
-        } else if (bmi < 18) {
-            msg = "痩せすぎです。";
-        } else if (bmi < 25) {
-            msg = "普通です。";
+
         } else {
-            msg = "肥満です。";
+            bmi = (int) (weight / Math.pow(height / 100.0, 2.0));
+            System.out.println("あなたのBMIは" + bmi + "です。");
+
+            if (bmi <= 0) {
+                msg = "身長または体重、あるいはその両方の入力が間違っています。";
+            } else if (bmi < 18) {
+                msg = "痩せすぎです。";
+            } else if (bmi < 25) {
+                msg = "普通です。";
+            } else {
+                msg = "肥満です。";
+            }
         }
         System.out.println(msg);
 
