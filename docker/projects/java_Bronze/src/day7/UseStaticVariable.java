0de798f8967995bb3f2273f4aacd98d445e4af6e
TkuUeno <sueno@cs.takushoku-u.ac.jp>
2023-12-23 10:14:24 +0900

    ???

--- docker/projects/java_Bronze/src/day7/UseStaticVariable.java
+++ docker/projects/java_Bronze/src/day7/UseStaticVariable.java
@@ -4,11 +4,11 @@ public class UseStaticVariable {
     public static void main(String[] args) {
 
         StaticVariable.num = 10; // インスタンス生成前に使用
-        StaticVariable sample = new StaticVariable();
+        // StaticVariable sample = new StaticVariable();
         System.out.println(StaticVariable.num);
 
-        sample.num = 11;
-        System.out.println(sample.num);
+        StaticVariable.num = 11;
+        System.out.println(StaticVariable.num);
     }
 
 }
